1d8f24d637bdef90c8797015d450c4e8c3063ee2
ayala_table <ayala941356@gmail.com>
2023-09-20 22:15:12 +0300

    יצירה של ראוטר וניתוב באינדקס

--- rauters/employees.js
+++ rauters/employees.js
@@ -1,3 +1,56 @@
 const express = require('express');
 const router = express.Router()
 module.exports = router;
+router.post("/Add",(req, res) => {
+    let name=req.body.name;
+
+    let q=`INSERT INTO \`employees\` (\`name\`) VALUES ('${name}')`;
+    db_pool.query(q, function(err, rows, fields){
+        if(err){
+            res.status(500).json({message: err})
+
+        }else{
+            res.status(200).json({message: "OK"});
+        }
+    });
+    // res.send("good morning");
+});
+router.patch("/Edit/:row_id",(req, res) => {
+    let id=req.params.row_id;
+    let name=req.body.name;
+    let q=`UPDATE \`employees\`  SET \`name\`='${name}' WHERE id=${id} `;
+    db_pool.query(q, function(err, rows, fields){
+        if(err){
+            res.status(500).json({message: err})
+            // throw err;
+        }else{
+            res.status(200).json({message: "OK"});
+        }
+    });
+});
+router.delete("/Del/:row_id",(req, res) => {
+    let id=req.params.row_id;
+    let q=`DELETE FROM \`employees\` WHERE id='${id}' `;
+    db_pool.query(q, function(err, rows, fields){
+        if(err){
+            res.status(500).json({message: err})
+            // throw err;
+        }else{
+            res.status(200).json({message: "OK"});
+        }
+    });
+});
+router.get("/List",(req, res) => {
+    let q="SELECT * FROM `employees` ";
+    db_pool.query(q, function(err, rows, fields){
+        if(err)
+        {
+            res.status(500).json({message: err})
+        }
+        else
+        {
+            res.status(200).json(rows );
+        }
+    });
+    // res.send("good morning");
+});
--- rauters/employees_time.js
+++ rauters/employees_time.js
@@ -1,3 +1,62 @@
 const express = require('express');
 const router = express.Router()
 module.exports = router;
+
+router.post("/Add",(req, res) => {
+   let {id ,time_start	}=req.body;
+    let q=`INSERT INTO \`employees_time\` (name,time_start) VALUES `;
+    q += `((SELECT name FROM employees WHERE id = ${id}),`;
+    q += `'${time_start	}')`;
+    db_pool.query(q, function(err, rows, fields){
+        if(err){
+            res.status(500).json({message: err})
+
+        }else{
+            res.status(200).json({message: "OK"});
+        }
+    });
+    // res.send("good morning");
+});
+
+
+router.patch("/Edit/:row_id",(req, res) => {
+    let id=req.params.row_id;
+    let time_end=req.body.time_end;
+    let q=`UPDATE employees  SET time_end ='${time_end}' WHERE id='${id}' `;
+    db_pool.query(q, function(err, rows, fields){
+        if(err){
+            res.status(500).json({message: err})
+            // throw err;
+        }else{
+            res.status(200).json({message: "OK"});
+        }
+    });
+});
+router.delete("/Del/:row_id",(req, res) => {
+    let id=req.params.row_id;
+    let q=`DELETE FROM \`employees\` WHERE id='${id}' `;
+    db_pool.query(q, function(err, rows, fields){
+        if(err){
+            res.status(500).json({message: err})
+            // throw err;
+        }else{
+            res.status(200).json({message: "OK"});
+        }
+    });
+});
+router.get("/List",(req, res) => {
+    let q="SELECT * FROM `employees` ";
+    db_pool.query(q, function(err, rows, fields){
+        if(err)
+        {
+            res.status(500).json({message: err})
+        }
+        else
+        {
+            res.status(200).json(rows );
+        }
+    });
+
+
+    // res.send("good morning");
+});
