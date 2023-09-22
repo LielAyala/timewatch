5473f79877a0607d44f303d2ee81e0d41249498c
ayala_table <ayala941356@gmail.com>
2023-09-22 13:53:41 +0300

    יצירת קבצי EJS

--- rauters/employees.js
+++ rauters/employees.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router()
 module.exports = router;
+router.get("/",function (req,res){
+    res.render("employees",{});
+})
 router.post("/Add",(req, res) => {
     let name=req.body.name;
 
@@ -15,8 +18,8 @@ router.post("/Add",(req, res) => {
     });
     // res.send("good morning");
 });
-router.patch("/Edit/:row_id",(req, res) => {
-    let id=req.params.row_id;
+router.patch("/Edit",(req, res) => {
+    let id=req.body.id;
     let name=req.body.name;
     let q=`UPDATE \`employees\`  SET \`name\`='${name}' WHERE id=${id} `;
     db_pool.query(q, function(err, rows, fields){
@@ -28,8 +31,9 @@ router.patch("/Edit/:row_id",(req, res) => {
         }
     });
 });
-router.delete("/Del/:row_id",(req, res) => {
-    let id=req.params.row_id;
+router.delete("/Del",(req, res) => {
+    // let id=req.params.row_id;
+    let id=req.body.id;
     let q=`DELETE FROM \`employees\` WHERE id='${id}' `;
     db_pool.query(q, function(err, rows, fields){
         if(err){
