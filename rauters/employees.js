3fcdd79abab716ba0666513d95988c76c84fadf1
ayala_table <ayala941356@gmail.com>
2023-09-23 21:55:49 +0300

    הוספת שאילה בADD שמוסיפה את העובד שנוסף לטבלה לטבלת העובדים עם הנוכחות

--- rauters/employees.js
+++ rauters/employees.js
@@ -6,7 +6,6 @@ router.get("/",function (req,res){
 })
 router.post("/Add",(req, res) => {
     let name=req.body.name;
-
     let q=`INSERT INTO \`employees\` (\`name\`) VALUES ('${name}')`;
     db_pool.query(q, function(err, rows, fields){
         if(err){
@@ -16,10 +15,17 @@ router.post("/Add",(req, res) => {
             res.status(200).json({message: "OK"});
         }
     });
-    // res.send("good morning");
-    // res.send("good morning");
+    let l=`INSERT INTO \`employees_time\` (\`name\`) VALUES ('${name}')`;
+    db_pool.query(l, function(err, rows, fields){
+        if(err){
+           res.status(500).json({message: err})
 
+        }else{
+            res.status(200).json({message: "OK"});
+        }
+    });
 });
+
 router.patch("/Edit",(req, res) => {
     let id=req.body.id;
     let name=req.body.name;
